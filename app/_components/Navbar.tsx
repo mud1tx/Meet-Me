77cbdefc6ca9179e9df9c5ccc7887e885be22040
mud1tx <muditagarwalna@gmail.com>
2023-12-07 22:12:17 +0530

    Navbar component added

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-9 xl:px-11 2xl:px-12 py-5 mb-4">
+      <div className="mx-auto">
+        <div className="text-2xl font-black">
+          VISTA
+        </div>
+        <div className="border-b-2 border-solid border-[#42433d] py-2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
