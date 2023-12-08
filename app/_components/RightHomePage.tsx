6ca6bf220f3915d63d2a2fa0fcdb07eafb4606d1
mud1tx <muditagarwalna@gmail.com>
2023-12-08 22:19:42 +0530

    Bug

--- app/_components/RightHomePage.tsx
+++ app/_components/RightHomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RightHomePage : React.FC= () => {
   return (
-    <div className="flex-shrink-0 w-1/2 p-4">RightHomePage</div>
+    <div className="flex-shrink-0 w-1/2 p-4">Remove</div>
   )
 }
 
