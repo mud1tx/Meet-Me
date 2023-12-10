bbeb4a5d00ab343bc8162ce668d55beb8ed98b2d
mud1tx <muditagarwalna@gmail.com>
2023-12-10 23:29:04 +0530

    added semi circle SVG

---
+++ app/_images/SemiCircleSVG.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+const SVGComponent = () => (
+  <svg
+    width={155}
+    height={124}
+    viewBox="0 0 155 124"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+  >
+    <path
+      d="M114.577 16.7765C114.176 17.0039 114.012 17.4907 114.19 17.9137C123.826 41.3135 115.82 68.0885 94.5769 80.0841C73.3337 92.0796 46.239 85.1288 31.1434 64.8042C30.8702 64.4357 30.3648 64.322 29.9641 64.5494L0.464816 81.2031C0.00488856 81.4624 -0.140804 82.0629 0.150635 82.4996C25.729 120.911 74.9457 134.708 113.256 113.087C151.567 91.4655 165.096 42.2187 145.351 0.518573C145.123 0.0409342 144.536 -0.141026 144.076 0.118264L114.577 16.7719V16.7765Z"
+      fill="url(#paint0_linear_2080_57114)"
+    />
+    <path
+      d="M114.577 16.7765C114.176 17.0039 114.012 17.4907 114.19 17.9137C123.826 41.3135 115.82 68.0885 94.5769 80.0841C73.3337 92.0796 46.239 85.1288 31.1434 64.8042C30.8702 64.4357 30.3648 64.322 29.9641 64.5494L0.464816 81.2031C0.00488856 81.4624 -0.140804 82.0629 0.150635 82.4996C25.729 120.911 74.9457 134.708 113.256 113.087C151.567 91.4655 165.096 42.2187 145.351 0.518573C145.123 0.0409342 144.536 -0.141026 144.076 0.118264L114.577 16.7719V16.7765Z"
+      fill="url(#pattern-home-animate-cutout-half-circle-0)"
+      fillOpacity={0.6}
+      style={{
+        mixBlendMode: "multiply",
+      }}
+    />
+    <defs>
+      <pattern
+        id="pattern-home-animate-cutout-half-circle-0"
+        patternContentUnits="objectBoundingBox"
+        width={1.29555}
+        height={1.62184}
+      >
+        <use xlinkHref="#svg-noise" transform="scale(0.00259111 0.00324368)" />
+      </pattern>
+      <linearGradient
+        id="paint0_linear_2080_57114"
+        x1={-18.9141}
+        y1={-15.6329}
+        x2={8.70042}
+        y2={120.212}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0.335249} stopColor="#CD237F" />
+        <stop offset={1} stopColor="#F7BDF8" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default SVGComponent;
