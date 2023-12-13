740627a4652ff8685d13bf1e380c4fb397d439b7
mud1tx <muditagarwalna@gmail.com>
2023-12-13 22:23:50 +0530

    bug fixes in animation of homepage

--- app/_components/HomePage.tsx
+++ app/_components/HomePage.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
-import RotatingFlowerSVG from "../_images/RotatingFlowerSVG";
 import RotatingChakra from "../../public/RotatingChakra.svg";
 import Image from "next/image";
 import FourLeafFlowerSVG from "../../public/FourLeafFlower.svg";
 import SandGlass from "../../public/SandGlass.svg";
 import Thunder from "../../public/Thunder.svg";
+import RotatingFlower from "../../public/RotatingFlower.svg";
 
 const HomePage: React.FC = () => {
   const [showSVG, setShowSVG] = useState(true);
@@ -22,7 +22,7 @@ const HomePage: React.FC = () => {
 
   useEffect(() => {
     const animateCharacterA = async () => {
-      await controlsA.start({ y: "40%", opacity: [0, 1, 0] });
+      await controlsA.start({ y: "100%", opacity: [0, 1, 0] });
     };
 
     const animateCharacterB = async () => {
@@ -96,14 +96,14 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center h-full flex-col md:px-12 lg:px-16 xl:px-20 2xl:px-24 px-6">
+    <div className="flex justify-center h-full flex-col md:px-12 lg:px-16 xl:px-20 2xl:px-24 px-6 mb-8">
       <div className="flex justify-center items-baseline">
         <div>
           <motion.span
             initial={{ x: "-100vw" }}
             animate={{ x: 0 }}
             transition={{ duration: 4, type: "spring", stiffness: 50 }}
-            className=" text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             V
           </motion.span>
@@ -113,15 +113,30 @@ const HomePage: React.FC = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2, ease: "easeInOut" }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             ı
           </motion.span>
-          <RotatingFlowerSVG />
+          <motion.span
+            initial={{ y: 100, opacity: 0 }}
+            animate={{
+              y: -20,
+              rotate: [0, 90],
+              opacity: [0, 1],
+            }}
+            transition={{
+              y: { duration: 1, type: "tween" },
+              rotate: { duration: 1, ease: "linear", repeat: Infinity },
+            }}
+            className="w-12 absolute"
+          >
+            <Image src={RotatingFlower} alt="Your Alt Text" className="w-12" />
+          </motion.span>
+          {/* <motion.span/> */}
         </div>
         {showSVG ? (
           <motion.span
-            className="inline-block"
+            className="w-12 inline-block"
             initial={{ opacity: 0, x: "-20vw" }}
             animate={{ opacity: 1, x: 0, rotate: [0, 360] }}
             transition={{
@@ -144,7 +159,7 @@ const HomePage: React.FC = () => {
             initial="hidden"
             animate="visible"
             transition={{ duration: 1, ease: "linear" }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             s
           </motion.span>
@@ -153,7 +168,7 @@ const HomePage: React.FC = () => {
           initial={{ opacity: 0, rotate: -90, originY: 0 }}
           animate={{ opacity: 1, rotate: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
         >
           u
         </motion.span>
@@ -161,7 +176,7 @@ const HomePage: React.FC = () => {
           <motion.div
             id="a"
             initial={{ y: "0%", opacity: 0 }}
-            animate={showSVG ? controlsA : { opacity: 0, y: "100%" }}
+            animate={showSVG ? controlsA : { opacity: 0, y: "0%" }}
             transition={{ duration: 2, ease: "easeInOut" }}
             className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block absolute"
             onAnimationComplete={handleAnimationComplete}
@@ -172,7 +187,7 @@ const HomePage: React.FC = () => {
             initial={{ y: "-100%", opacity: 0 }}
             animate={controlsB}
             transition={{ duration: 2, ease: "easeInOut" }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block absolute"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             a
           </motion.div>
@@ -183,7 +198,7 @@ const HomePage: React.FC = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block ml-12 min-[500px]:ml-14"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             00
           </motion.div>
@@ -197,7 +212,7 @@ const HomePage: React.FC = () => {
               stiffness: 10,
               damping: 25,
             }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block ml-12 min-[500px]:ml-14"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             l
           </motion.div>
@@ -208,7 +223,7 @@ const HomePage: React.FC = () => {
           initial={{ opacity: 0, scaleY: 0, originY: 1 }}
           animate={{ opacity: 1, scaleY: 1, originY: 0 }}
           transition={{ type: "spring", stiffness: 50, damping: 20 }}
-          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
         >
           V
         </motion.span>
@@ -223,7 +238,7 @@ const HomePage: React.FC = () => {
           variants={animationVariants}
           initial="hidden"
           animate="visible"
-          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+          className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
         >
           s
         </motion.span>
@@ -245,7 +260,7 @@ const HomePage: React.FC = () => {
             initial="hidden"
             animate="visible"
             transition={{ duration: 2, ease: "linear" }}
-            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[118px] min-[500px]:text-[99px] md:text-[134.4px] lg:text-[168.5px] xl:text-[202.6px] min-[1440px]:text-[224px] 2xl:text-[236.8px] font-bold inline-block"
+            className="text-[5.2rem] min-[425px]:text-[88.66px] sm:text-[134px] min-[500px]:text-[118px] md:text-[168px] lg:text-[200px] xl:text-[224px] min-[1440px]:text-[236px] font-bold inline-block"
           >
             t
           </motion.div>
